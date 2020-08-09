1c385b6dbaf6a4ad86c5905e0582e85e632cbdc9
Creator <gtarasu1993@gmail.com>
2020-08-09 19:59:44 +0530

    Login Page Initial Changes
    Login Page Initial Changes

--- CRUD Application/CRUD/App_Start/BundleConfig.cs
+++ CRUD Application/CRUD/App_Start/BundleConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System.Diagnostics.SymbolStore;
+using System.Web;
 using System.Web.Optimization;
 
 namespace CRUD
@@ -10,22 +11,24 @@ namespace CRUD
 		{
 			bundles.Add(new ScriptBundle("~/bundles/Jquery").Include(
 						//"~/Scripts/Framework/jQuery/jquery-3.4.1.intellisense.js",
-						"~/Scripts/Framework/jQuery/jquery-3.4.1.js",
-						"~/Scripts/Framework/jQuery/jquery-3.4.1.min.js",
-						"~/Scripts/Framework/jQuery/jquery-3.4.1.min.map",
-						"~/Scripts/Framework/jQuery/jquery-3.4.1.slim.js",
-						"~/Scripts/Framework/jQuery/jquery-3.4.1.slim.min.js",
-						"~/Scripts/Framework/jQuery/jquery-3.4.1.slim.min.map",
-						"~/Scripts/Framework/jQuery/jquery.validate-vsdoc.js",
-						"~/Scripts/Framework/jQuery/jquery.validate.js",
-						"~/Scripts/Framework/jQuery/jquery.validate.min.js",
-						"~/Scripts/Framework/jQuery/jquery.validate.unobtrusive.js",
-						"~/Scripts/Framework/jQuery/jquery.validate.unobtrusive.min.js"
+						"~/Scripts/Framework/jQuery/jquery-3.4.1.js"
+						//"~/Scripts/Framework/jQuery/jquery-3.4.1.js",
+						//"~/Scripts/Framework/jQuery/jquery-3.4.1.min.js",
+						//"~/Scripts/Framework/jQuery/jquery-3.4.1.min.map",
+						//"~/Scripts/Framework/jQuery/jquery-3.4.1.slim.js",
+						//"~/Scripts/Framework/jQuery/jquery-3.4.1.slim.min.js",
+						//"~/Scripts/Framework/jQuery/jquery-3.4.1.slim.min.map",
+						//"~/Scripts/Framework/jQuery/jquery.validate-vsdoc.js",
+						//"~/Scripts/Framework/jQuery/jquery.validate.js",
+						//"~/Scripts/Framework/jQuery/jquery.validate.min.js",
+						//"~/Scripts/Framework/jQuery/jquery.validate.unobtrusive.js",
+						//"~/Scripts/Framework/jQuery/jquery.validate.unobtrusive.min.js"
 						));
 
 			bundles.Add(new ScriptBundle("~/bundles/Bootstrap").Include(
-						"~/Scripts/Framework/Bootstrap/bootstrap.js",
-						"~/Scripts/Framework/Bootstrap/bootstrap.min.js"
+						"~/Scripts/Framework/Bootstrap/bootstrap.js"
+						//"~/Scripts/Framework/Bootstrap/bootstrap.js",
+						//"~/Scripts/Framework/Bootstrap/bootstrap.min.js"
 						));
 
 			// Use the development version of Modernizr to develop with and learn from. Then, when you're
@@ -40,15 +43,19 @@ namespace CRUD
 
 			bundles.Add(new StyleBundle("~/bundles/Css").Include(
 					  "~/Content/Bootstrap/bootstrap-theme.css",
-					  "~/Content/Bootstrap/bootstrap-theme.css.map",
-					  "~/Content/Bootstrap/bootstrap-theme.min.css",
-					  "~/Content/Bootstrap/bootstrap-theme.min.css.map",
+					  //"~/Content/Bootstrap/bootstrap-theme.css.map",
+					  //"~/Content/Bootstrap/bootstrap-theme.min.css",
+					  //"~/Content/Bootstrap/bootstrap-theme.min.css.map",
+					  //"~/Content/Bootstrap/bootstrap.css",
+					  //"~/Content/Bootstrap/bootstrap.css.map",
+					  //"~/Content/Bootstrap/bootstrap.min.css",
+					  //"~/Content/Bootstrap/bootstrap.min.css.map",
 					  "~/Content/Bootstrap/bootstrap.css",
-					  "~/Content/Bootstrap/bootstrap.css.map",
-					  "~/Content/Bootstrap/bootstrap.min.css",
-					  "~/Content/Bootstrap/bootstrap.min.css.map",
 					  "~/Content/Site.css"
 					  ));
+			bundles.Add(new StyleBundle("~/bundles/AppCss").Include(
+				 "~/Content/Application/*.css"
+				));
 		}
 	}
 }
